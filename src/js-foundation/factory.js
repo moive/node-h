4183c1546f1c07fa08031ac656ba0b0991b53ade
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2023-12-11 23:20:26 -0500

    adapter pattern

--- src/js-foundation/factory.js
+++ src/js-foundation/factory.js
@@ -1,11 +1,10 @@
-const { v4: uuidv4 } = require("uuid");
-const getAge = require("get-age");
+const { getUUID, getAge } = require("../plugins");
 
 const obj = { name: "John", birdhdate: "1981-12-30" };
 
 const buildPerson = ({ name, birdhdate }) => {
 	return {
-		id: uuidv4(),
+		id: getUUID(),
 		name,
 		birdhdate,
 		age: getAge(birdhdate),
