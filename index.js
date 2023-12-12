d223380ce7af56d970ccf670bc3057704ae49296
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2023-12-11 22:26:37 -0500

    callback

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-const { emailTemlate } = require("./js-foundation/template");
-require("./js-foundation/destructuring");
+// const { emailTemlate } = require("./js-foundation/template");
+// require("./js-foundation/destructuring");
+const { getUserById } = require("./js-foundation/callback");
+
+const id = 1;
+
+getUserById(id, function (error, user) {
+	if (error) {
+		throw new Error(error);
+	}
+	console.log(user);
+});
