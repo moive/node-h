18ea18411dd435773421d2fd1f8bc17418cae402
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2023-12-11 22:35:17 -0500

    arrow functions

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 // const { emailTemlate } = require("./js-foundation/template");
 // require("./js-foundation/destructuring");
-const { getUserById } = require("./js-foundation/callback");
+// const { getUserById } = require("./js-foundation/callback");
+const { getUserById } = require("./js-foundation/arrow");
 
 const id = 1;
 
-getUserById(id, function (error, user) {
-	if (error) {
-		throw new Error(error);
-	}
+getUserById(id, (error, user)=> {
+	if (error) throw new Error(error);
 	console.log(user);
 });
