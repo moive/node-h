aab78fece364a7c3f5961fe7ac14ee6fd1247091
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2024-02-06 23:48:32 -0500

    promise with callback and string

--- src/js-foundation/apiPokemon.js
+++ src/js-foundation/apiPokemon.js
@@ -1,11 +1,9 @@
 const getPokemonById = (id, callback) => {
 	const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
 
-	fetch(url).then((response) => {
-		response.json().then((pokemon) => {
-			callback(pokemon.name);
-		});
-	});
+	fetch(url)
+		.then((response) => response.json())
+		.then((pokemon) => callback(pokemon.name));
 	// return "Pokemon";
 };
 
