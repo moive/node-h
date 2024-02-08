0e3afd86fbe247e378863dd4ea228013bbe47fcb
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2024-02-08 12:37:51 -0500

    add timestamp in logger

--- src/plugins/logger.plugin.js
+++ src/plugins/logger.plugin.js
@@ -1,8 +1,9 @@
 const winston = require("winston");
+const { combine, timestamp, json } = winston.format;
 
 const logger = winston.createLogger({
 	level: "info",
-	format: winston.format.json(),
+	format: combine(timestamp(), json()),
 	// defaultMeta: { service: "user-service" },
 	transports: [
 		//
